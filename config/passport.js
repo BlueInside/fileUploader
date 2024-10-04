8a051a97aba5f5b167f16732bbb01a1178802e33
Karol <kpulawski13@gmail.com>
2024-10-04 12:28:48 +0100

    add verify function to passport config

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,31 @@
 const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcryptjs');
+const prisma = require('../prisma');
+
+const verifyFunction = async (username, password, done) => {
+    try {
+
+        const user = await prisma.users.findUnique({
+            where: { username: username }
+        });
+
+        if (!user) {
+            return done(null, false, { message: 'Incorrect username.' })
+        }
+
+        const match = await bcrypt.compare(password, user.password)
+        if (!match) {
+            return done(null, false, { message: 'Incorrect password.' })
+        }
+        return done(null, user)
+    }
+    catch (err) {
+        return done(err);
+    }
+}
+
+passport.use(new LocalStrategy(verifyFunction))
+
+module.exports = passport
+
