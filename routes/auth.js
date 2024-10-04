1de28daf372499579adae5d5d1d59bb632028d69
Karol <kpulawski13@gmail.com>
2024-10-04 14:32:18 +0100

    add validations

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const authRouter = express.Router();
 const authController = require('../controllers/authController');
+const { registerValidations, validateRegistration } = require('../validators/registerValidations')
+const { loginValidations, validateLogin } = require('../validators/loginValidations')
 
 authRouter.get('/login', authController.getLoginPage)
-authRouter.post('/login', authController.userLogin)
+authRouter.post('/login', loginValidations, validateLogin, authController.userLogin)
 
 authRouter.get('/register', authController.getRegisterPage)
-authRouter.post('/register', authController.userRegister)
+authRouter.post('/register', registerValidations, validateRegistration, authController.userRegister)
 
 module.exports = authRouter
