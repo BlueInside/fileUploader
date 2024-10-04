c1aeaed2702ced7d2589e96279b574bb9cff3021
Karol <kpulawski13@gmail.com>
2024-10-04 10:35:06 +0100

    start implementing login and register controllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler');
+const prisma = require('../prisma');
 
 const getLoginPage = asyncHandler(async (req, res, next) => {
     res.render('login')
@@ -8,7 +9,18 @@ const getRegisterPage = asyncHandler(async (req, res, next) => {
     res.render('register')
 })
 
+const userLogin = asyncHandler(async((req, res, next) => {
+    res.send('IMPLEMENT USER LOGIN POST')
+}))
+
+const userRegister = asyncHandler(async (req, res, next) => {
+    const { email: username, password, confirm_password } = req.body
+    console.log('REGISTER ', username, password, confirm_password)
+    res.redirect('/login')
+})
 module.exports = {
     getLoginPage,
     getRegisterPage,
+    userLogin,
+    userRegister
 }
