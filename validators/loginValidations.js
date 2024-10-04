8fc47e69b9220aa17e1cbe41ff76648436674a66
Karol <kpulawski13@gmail.com>
2024-10-04 14:32:28 +0100

    implement validations

--- validators/loginValidations.js
+++ validators/loginValidations.js
@@ -1,1 +1,21 @@
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator')
+
+const validateLogin = (req, res, next) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        return res.render('login', { errors: result.array() })
+    }
+    next();
+}
+
+const loginValidations = [
+    body('username')
+        .isEmail()
+        .withMessage('Please enter a valid email address.')
+        .normalizeEmail(),
+
+    body('password')
+        .isLength({ min: 6, max: 50 }).withMessage('Password must be 6-50 characters long'),
+]
+
+module.exports = { validateLogin, loginValidations };
---
+++ validators/registerValidations.js
@@ -0,0 +1,28 @@
+const { body, validationResult } = require('express-validator')
+
+const validateRegistration = (req, res, next) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        return res.render('register', { errors: result.array() })
+    }
+    next();
+}
+
+const registerValidations = [
+    body('username')
+        .isEmail()
+        .withMessage('Please enter a valid email address.')
+        .normalizeEmail(),
+
+    body('password')
+        .isLength({ min: 6, max: 50 }).withMessage('Password must be 6-50 characters long'),
+    body('confirm_password')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error(`Passwords don't match`);
+            }
+            return true
+        }),
+]
+
+module.exports = { registerValidations, validateRegistration }
