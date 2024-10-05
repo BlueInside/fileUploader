643ce9bcaa59cafab340d3387fea711fdd0976a4
Karol <kpulawski13@gmail.com>
2024-10-05 21:04:01 +0100

    add controller to login

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,11 @@ const authRouter = express.Router();
 const authController = require('../controllers/authController');
 const { registerValidations, validateRegistration } = require('../validators/registerValidations')
 const { loginValidations, validateLogin } = require('../validators/loginValidations')
+const passport = require('passport');
 
 authRouter.get('/login', authController.getLoginPage)
-authRouter.post('/login', loginValidations, validateLogin, authController.userLogin)
+authRouter.post('/login', loginValidations, authController.userLogin)
+
 
 authRouter.get('/register', authController.getRegisterPage)
 authRouter.post('/register', registerValidations, validateRegistration, authController.userRegister)
