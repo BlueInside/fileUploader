2d860818fcec735cdcd85aaa6ed1c9de099f2e1a
Karol <kpulawski13@gmail.com>
2024-10-06 10:06:05 +0100

    create dummy fileController

---
+++ controllers/filesController.js
@@ -0,0 +1,27 @@
+const multer = require('multer');
+
+
+const upload = multer({ dest: 'uploads/' });
+
+const getFiles = (req, res, next) => {
+    res.send('GET ALL FILES')
+}
+
+const addFile = (req, res, next) => {
+    res.send('POST FILE')
+}
+
+const updateFile = (req, res, next) => {
+    res.send(`UPDATE ${req.params.id} FILE`)
+}
+
+const removeFile = (req, res, next) => {
+    res.send(`REMOVE ${req.params.id} FILE`)
+}
+
+module.exports = {
+    getFiles,
+    addFile,
+    updateFile,
+    removeFile,
+}
