61601338030d84111c50f93fa2fcad120b6a6bcf
Karol <kpulawski13@gmail.com>
2024-10-06 11:35:42 +0100

    validate login and ensure Authenticated

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,14 @@ const authRouter = express.Router();
 const authController = require('../controllers/authController');
 const { registerValidations, validateRegistration } = require('../validators/registerValidations')
 const { loginValidations, validateLogin } = require('../validators/loginValidations')
-const passport = require('passport');
+const { ensureAuthenticated } = require('../middlewares/authMiddleware')
 
 authRouter.get('/login', authController.getLoginPage)
-authRouter.post('/login', loginValidations, authController.userLogin)
+authRouter.post('/login', loginValidations, validateLogin, authController.userLogin)
 
 
 authRouter.get('/register', authController.getRegisterPage)
 authRouter.post('/register', registerValidations, validateRegistration, authController.userRegister)
 
-authRouter.get('/logout', authController.userLogout)
+authRouter.get('/logout', ensureAuthenticated, authController.userLogout)
 module.exports = authRouter
