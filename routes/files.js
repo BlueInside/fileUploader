e6ee21c965d97b08ada28f7c6275d0011c790ccb
Karol <kpulawski13@gmail.com>
2024-10-06 11:35:59 +0100

    remove post and put operations from file router

--- routes/files.js
+++ routes/files.js
@@ -6,10 +6,6 @@ const fileRouter = express.Router();
 
 fileRouter.get('/', filesController.getFiles)
 
-fileRouter.post('/', upload.single('file'), filesController.createFile)
-
-fileRouter.put('/', filesController.updateFile)
-
 fileRouter.delete('/', filesController.removeFile)
 
 module.exports = fileRouter
