329272bfcf6899f6ec53bbde07f35c22b18406ad
Karol <kpulawski13@gmail.com>
2024-10-13 10:00:16 +0100

    fetch folder info and renders update form view

--- validators/updateFormValidations.js
+++ validators/updateFormValidations.js
@@ -1,9 +1,33 @@
 const { body, validationResult } = require('express-validator')
+const prisma = require('../prisma');
 
-const validateUpdateForm = (req, res, next) => {
+const validateUpdateForm = async (req, res, next) => {
     const result = validationResult(req);
     if (!result.isEmpty()) {
-        return res.render('login', { errors: result.array() })
+
+        const folderId = parseInt(req.body.folder_id, 10);  // Convert folder_id to a number
+
+        if (isNaN(folderId)) {
+            return next(new Error('Invalid folder ID'));
+        }
+
+        let folderData;
+
+        try {
+            folderData = await prisma.folder.findUnique({
+                where: { id: folderId }
+            });
+
+
+            if (!folderData) {
+                throw new Error('Failed to fetch folder information')
+            }
+
+        } catch (error) {
+            next(error)
+        }
+
+        return res.render('editFolderForm', { folderData, errors: result.array() })
     }
     next();
 }
@@ -14,7 +38,11 @@ const updateFolderValidations = [
         .notEmpty()
         .withMessage('Folder name cannot be empty')
         .isLength({ min: 1, max: 15 })
+        .withMessage('Folder must be between 1-15 characters long')
         .escape(),
+    body('folder_id')
+        .notEmpty()
+        .withMessage('Invalid folder identifier')
 ]
 
 module.exports = { validateUpdateForm, updateFolderValidations };
