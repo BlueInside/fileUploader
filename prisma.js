ef51d53380ef022a47183a6601c288fb7f6098f3
Karol <kpulawski13@gmail.com>
2024-10-25 12:42:19 +0100

    create file in db

--- prisma.js
+++ prisma.js
@@ -1,25 +1,39 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient()
 
-// async function main() {
-//     const users = await prisma.user.findFirst({
-//         include: {
-//             files: true,
-//             folders: true,
-//         }
-//     });
+async function main() {
+    const users = await prisma.user.findFirst({
+        include: {
+            files: true,
+            folders: true,
+        }
+    });
 
-//     console.log(users);
-// }
+    console.log(users);
 
-// main()
-//     .then(async () => {
-//         await prisma.$disconnect()
-//     })
-//     .catch(async (e) => {
-//         console.error(e)
-//         await prisma.$disconnect()
-//         process.exit(1)
-//     })
+    // const file = await prisma.file.create({
+    //     data: {
+    //         fileName: 'Samurai_Jack',
+    //         size: 12450,
+    //         url: 'downloadString',
+    //         folderId: 2,
+    //         userId: 1
+    //     }
+    // });
+
+
+    // console.log(file);
+
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
 
 module.exports = prisma
