17c220cdabe1f9b1493136fc89bdb1cfedaf4e43
Karol <kpulawski13@gmail.com>
2024-10-25 12:55:54 +0100

    add isFileOwner middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,3 +1,5 @@
+const prisma = require('../prisma');
+
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
@@ -6,4 +8,40 @@ function ensureAuthenticated(req, res, next) {
     res.redirect('/login');
 }
 
-module.exports = { ensureAuthenticated }
+async function isFileOwner(req, res, next) {
+    try {
+        const fileId = parseInt(req.params.id, 10);
+
+        if (isNaN(fileId)) {
+            const error = new Error('Invalid file identifier!');
+            error.status = 400;
+            throw error;
+        }
+
+        const file = await prisma.file.findUnique({
+            where: { id: req.params.id },
+            select: {
+                userId: true,
+            }
+        })
+
+        if (!file) {
+            const error = new Error('File not found!');
+            error.status = 404;
+            throw error;
+        }
+
+        if (file.userId !== req.user.id) {  // Ensure the file's owner is the logged-in user
+            const error = new Error('Not authorized to view this file');
+            error.status = 403;
+            throw error;
+        }
+
+        return next();
+    } catch (error) {
+        return next(error);
+    }
+}
+
+
+module.exports = { ensureAuthenticated, isFileOwner }
