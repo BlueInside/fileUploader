455e2eb374bfb8524e08845efda060c3ba39a4a5
Karol <kpulawski13@gmail.com>
2024-10-28 07:38:42 +0000

    route to upload files

--- routes/files.js
+++ routes/files.js
@@ -4,9 +4,12 @@ const filesController = require('../controllers/filesController');
 const upload = multer({ dest: 'uploads/' });
 const fileRouter = express.Router();
 
-const { ensureAuthenticated, isFileOwner } = require('../middlewares/authMiddleware');
+const { ensureAuthenticated, isFileOwner, isFolderOwner } = require('../middlewares/authMiddleware');
 
 fileRouter.get('/', ensureAuthenticated, filesController.getFiles)
+
+// Add file to specific folder
+fileRouter.post('/', ensureAuthenticated, upload.single('file'), isFolderOwner, filesController.uploadFile)
 
 fileRouter.get('/:id/details', ensureAuthenticated, filesController.getFileInfo)
 
